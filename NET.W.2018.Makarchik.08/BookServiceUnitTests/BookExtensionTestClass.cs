43ecfa480aa9965ccf07b1f0147ace1116a62ecb
AliceInHell <35768088+AliceInHell@users.noreply.github.com>
2018-11-04 16:20:21 +0300

    Added BookService tests

---
+++ NET.W.2018.Makarchik.08/BookServiceUnitTests/BookExtensionTestClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BookService;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookServiceUnitTests
+{
+    [TestClass]
+    public class BookExtensionTestClass
+    {
+        [TestMethod]
+        public void BookExtensionTest()
+        {
+            Book book = new Book("ISBN", "Title", "Author", "PublishingHous",
+                2005, 200, 50);
+
+            string expected = "Book record: Author, Title, 200";
+
+            Assert.IsTrue(book.NewFormat() == expected);
+        }
+
+        [TestMethod]
+        public void BookToStringMethodTest()
+        {
+            Book book = new Book("ISBN", "Title", "Author", "PublishingHous",
+                2005, 200, 50);
+
+            string expected = "Book record: Author, Title";
+
+            Assert.IsTrue(book.ToString(new TitleAndAuthorFormat()) == expected);
+        }
+    }
+}
